bb799ae29cc322ad73fbd49c314cd4828e0bbb26
Alexandre Sonier <alexandre.sonier@gmail.com>
2015-09-11 06:08:50 +0800

    Create notification + push notification to socket.io

---
+++ src/AppBundle/Controller/NotificationController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Util\Codes;
+use FOS\RestBundle\Controller\Annotations;
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+use AppBundle\Entity\Notification;
+use AppBundle\Entity\Notification_User;
+
+/**
+ * User controller
+ *
+ * @Route("/notification")
+ */
+class NotificationController extends FOSRestController
+{
+    /**
+     * Create a notification
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   description = "Create a notification and pushes it to the server",
+     *   output = "string",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     500 = "Returned when something went wrong"
+     *   }
+     * )
+     *
+     *
+     * @param Request $request
+     *
+     * @return void
+     *
+     * @throws AccessDeniedException when something went wrong
+     *
+     * @Route("/create", name="create")
+     * @Method("POST")
+     */
+    public function createAction(Request $request)
+    {
+        $token = $request->request->get('token');
+        $title = $request->request->get('title');
+        $content = $request->request->get('content');
+        $actionUrl = $request->request->get('actionUrl');
+        $actionCaption = $request->request->get('actionCaption') ?: 'More Details';
+
+        if (!$token || !$title) {
+            throw $this->createNotFoundException('Required parameter(s) missing');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('AppBundle:User')->findOneByToken($token);
+
+        if (!$user) {
+            throw $this->createNotFoundException('Unknown token');
+        }
+
+        $n = $this->saveNotification($token, $title, $actionCaption, $content, $actionUrl);
+        $this->sendNotification($user->getIp(), $n, $actionUrl);
+
+        $view = $this->view(true, 200)->setFormat('json');
+
+        return $view;
+    }
+
+    /**
+     * Save notification in DB
+     *
+     * @param  string $token
+     * @param  string $title
+     * @param  string $actionCaption
+     * @param  string $content
+     * @param  string $actionUrl
+     * @return Notification
+     */
+    private function saveNotification($token, $title, $actionCaption, $content = null, $actionUrl = null)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository('AppBundle:User')->findOneByToken($token);
+        $notification = new Notification();
+        $notification->setTitle($title)
+                     ->setContent($content)
+                     ->setType(0)
+                     ->setActionCaption($actionCaption)
+        ;
+        $em->persist($notification);
+        $em->flush();
+
+        $notificationUser = new Notification_User();
+        $notificationUser->setNotification($notification)
+                         ->setUser($user)
+                         ->setCreatedTime(new \DateTime())
+                         ->setActionUrl($actionUrl)
+                         ->setIsClicked(false)
+        ;
+
+        $em->persist($notificationUser);
+        $em->flush();
+
+        return $notification;
+    }
+
+    /**
+     * Sends a notification through socket.io
+     *
+     * @param  string       $ip
+     * @param  Notification $notification
+     * @param  string       $actionUrl
+     * @return void
+     */
+    private function sendNotification($ip, $notification, $actionUrl = null)
+    {
+        $data = array(
+            'ip'            => $ip,
+            'title'         => $notification->getTitle(),
+            'content'       => $notification->getContent(),
+            'actionUrl'     => $actionUrl,
+            'actionCaption' => $notification->getActionCaption()
+        );
+
+        $socketClient = $this->get('elephantio_client.ainotifier');
+        $socketClient->send('new', ['notification' => json_encode($data)]);
+    }
+}
--- src/AppBundle/Entity/Notification_User.php
+++ src/AppBundle/Entity/Notification_User.php
@@ -43,4 +43,142 @@ class Notification_User
      * @ORM\Column(name="is_clicked", type="boolean", nullable=false)
      */
     protected $isClicked;
+
+    /**
+     * Set createdTime
+     *
+     * @param \DateTime $createdTime
+     * @return Notification_User
+     */
+    public function setCreatedTime($createdTime)
+    {
+        $this->createdTime = $createdTime;
+
+        return $this;
+    }
+
+    /**
+     * Get createdTime
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedTime()
+    {
+        return $this->createdTime;
+    }
+
+    /**
+     * Set pushedTime
+     *
+     * @param \DateTime $pushedTime
+     * @return Notification_User
+     */
+    public function setPushedTime($pushedTime)
+    {
+        $this->pushedTime = $pushedTime;
+
+        return $this;
+    }
+
+    /**
+     * Get pushedTime
+     *
+     * @return \DateTime 
+     */
+    public function getPushedTime()
+    {
+        return $this->pushedTime;
+    }
+
+    /**
+     * Set actionUrl
+     *
+     * @param string $actionUrl
+     * @return Notification_User
+     */
+    public function setActionUrl($actionUrl)
+    {
+        $this->actionUrl = $actionUrl;
+
+        return $this;
+    }
+
+    /**
+     * Get actionUrl
+     *
+     * @return string 
+     */
+    public function getActionUrl()
+    {
+        return $this->actionUrl;
+    }
+
+    /**
+     * Set isClicked
+     *
+     * @param boolean $isClicked
+     * @return Notification_User
+     */
+    public function setIsClicked($isClicked)
+    {
+        $this->isClicked = $isClicked;
+
+        return $this;
+    }
+
+    /**
+     * Get isClicked
+     *
+     * @return boolean 
+     */
+    public function getIsClicked()
+    {
+        return $this->isClicked;
+    }
+
+    /**
+     * Set notification
+     *
+     * @param \AppBundle\Entity\Notification $notification
+     * @return Notification_User
+     */
+    public function setNotification(\AppBundle\Entity\Notification $notification)
+    {
+        $this->notification = $notification;
+
+        return $this;
+    }
+
+    /**
+     * Get notification
+     *
+     * @return \AppBundle\Entity\Notification 
+     */
+    public function getNotification()
+    {
+        return $this->notification;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \AppBundle\Entity\User $user
+     * @return Notification_User
+     */
+    public function setUser(\AppBundle\Entity\User $user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \AppBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
 }
